0ef169aa41279c912e943d01d34fe3d8524fc5d9
Aurelio Munoz Hijano <aurelio@sparanzza.com>
2023-11-04 10:26:26 +0100

    C02 - update raster_images.py with another sprite in front layer

--- Chapter_Second/raster_images.py
+++ Chapter_Second/raster_images.py
@@ -9,11 +9,15 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('A beautiful sunset')
 done = False
 
-background = pygame.image.load('img.png')
+background = pygame.image.load('../images/sunset.png')
+sprite = pygame.image.load('../images/ryu.png')
+sprite = pygame.transform.scale(sprite, (sprite.get_width() // 2, sprite.get_height() // 2))
+
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
     screen.blit(background, (0, 0))
+    screen.blit(sprite, (400, 400))
     pygame.display.update()
 pygame.quit()
